fb8b0093168a0477b3d14ae0f744cbabe9b28fc1
Andres Gomez <andrefelgoal@gmail.com>
2020-12-10 09:17:39 -0500

    servicio actualizar

--- src/main/java/com/clearminds/smo/AFGA/servicios/RestEstudiantes.java
+++ src/main/java/com/clearminds/smo/AFGA/servicios/RestEstudiantes.java
@@ -2,6 +2,7 @@ package com.clearminds.smo.AFGA.servicios;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 
@@ -22,5 +23,18 @@ public class RestEstudiantes {
 		} catch (BDDException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	@PUT
+	@Path("actualizar")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void actualizar(Estudiante estudiante){
+		ServicioEstudiante servicioEstudiante = new ServicioEstudiante();
+		try{
+			servicioEstudiante.actualizarEstudiante(estudiante);
+		} catch (BDDException e) {
+			e.printStackTrace();
+		}
+		
 	}
 }
