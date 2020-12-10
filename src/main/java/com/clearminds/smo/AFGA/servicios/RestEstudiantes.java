0d473ce6bee54e55e4f51dacac488d7d0051c0eb
Andres Gomez <andrefelgoal@gmail.com>
2020-12-09 19:38:00 -0500

    Servicio registrar

---
+++ src/main/java/com/clearminds/smo/AFGA/servicios/RestEstudiantes.java
@@ -0,0 +1,26 @@
+package com.clearminds.smo.AFGA.servicios;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+
+import com.clearminds.AFGA.dtos.Estudiante;
+import com.clearminds.AFGA.excepciones.BDDException;
+import com.clearminds.AFGA.servicios.ServicioEstudiante;
+
+@Path("estudiantes")
+public class RestEstudiantes {
+	
+	@POST
+	@Path("insertar")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void insertar(Estudiante estudiante){
+		ServicioEstudiante servicioEstudiante = new ServicioEstudiante();
+		try {
+			servicioEstudiante.insertarEstudiante(estudiante);
+		} catch (BDDException e) {
+			e.printStackTrace();
+		}
+	}
+}
